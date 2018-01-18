c4ffa2f6f1fb980c700a9e5b3f22de240f56d34f
Peter Street <PeterBStreet@gmail.com>
2018-01-17 22:59:56 -0700

    Updated directory structure and read me

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # personal-website
+This is Peter Street's Personal website
+My goal is to have a great web presence.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petersdata
+ * Date: 1/17/18
+ * Time: 10:53 PM
+ */
+echo "I would like potential employers to look at my site"
