068c78aaa090521f3287cb3638484a536f280bd2
Peter Street <PeterBStreet@gmail.com>
2018-01-18 10:08:04 -0700

    Moved Mission Statement, Audiance, Goals, and Personal from MSWord to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,4 +5,11 @@
  * Date: 1/17/18
  * Time: 10:53 PM
  */
-<p>"I would like potential employers to look at my site"</h>
+
+"I would like my website to provide viewers a place to highlight my portfolio of completed projects demonstrationg my capabilities. Ultimatly, I want my personal website to be a part of a unified marketing message including LinkedIn and github."
+
+"The audiance for my personal website is potential customers of my consulting business, employeers, peers, and mentors."
+
+"The goal of my personal website is to acquire employment, either full or part time contract or permenant. Additionally the website should motivate clients to work with my firm (read increase close ratio)."
+
+"Ricardo, is a business owner and entrepenure living in Albuquerque NM. He runs a successful ISP and is interested in starting a new Etherum Coin Mining facility but he realizes that he needs to set up a WISP to fund his mining pet project. Ricardo needs help setting up his new businesses and needs to streamline his existing business. Ricardo is frustrated because he is having issues with operations at his current company and has two large projects that require formal project management. So far he is having trouble finding an employee with strong business and strategic planning skills who can help him improve the processes at his existing company, and help him plan and implement his IT startups."
