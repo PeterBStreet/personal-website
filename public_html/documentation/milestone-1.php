46d65d0c3ba764348817aee655ce0170b2dca67a
Peter Street <PeterBStreet@gmail.com>
2018-01-18 10:37:52 -0700

    php to html milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,15 +1,43 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: petersdata
- * Date: 1/17/18
- * Time: 10:53 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>personal-website</title>
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	</head>
+	<body>
+		<div>
+			<p>
+				I would like my website to provide viewers a place to highlight my portfolio of completed projects
+				demonstrationg my capabilities. Ultimatly, I want my personal website to be a part of a unified marketing
+				message including LinkedIn and github.
+			</p>
+		</div>
+		<div>
+			<p>
+				The audiance for my personal website is potential customers of my consulting business, employeers, peers,
+				and mentors.
+			</p>
+		</div>
+		<div>
+			<p>
+				The goal of my personal website is to acquire employment, either full or part time contract or permenant.
+				Additionally the website should motivate clients to work with my firm (read increase close ratio).
+			</p>
+		</div>
+		<div>
+			Ricardo, is a business owner and entrepenure living in Albuquerque NM. He runs a successful ISP and is
+			interested in starting a new Etherum Coin Mining facility but he realizes that he needs to set up a WISP to
+			fund his mining pet project. Ricardo needs help setting up his new businesses and needs to streamline his
+			existing business. Ricardo is frustrated because he is having issues with operations at his current company and
+			has two large projects that require formal project management. So far he is having trouble finding an employee
+			with strong business and strategic planning skills who can help him improve the processes at his existing
+			company, and help him plan and implement his IT startups.
+		</div>
+	</body>
+</html>
 
-"I would like my website to provide viewers a place to highlight my portfolio of completed projects demonstrationg my capabilities. Ultimatly, I want my personal website to be a part of a unified marketing message including LinkedIn and github."
 
-"The audiance for my personal website is potential customers of my consulting business, employeers, peers, and mentors."
 
-"The goal of my personal website is to acquire employment, either full or part time contract or permenant. Additionally the website should motivate clients to work with my firm (read increase close ratio)."
 
-"Ricardo, is a business owner and entrepenure living in Albuquerque NM. He runs a successful ISP and is interested in starting a new Etherum Coin Mining facility but he realizes that he needs to set up a WISP to fund his mining pet project. Ricardo needs help setting up his new businesses and needs to streamline his existing business. Ricardo is frustrated because he is having issues with operations at his current company and has two large projects that require formal project management. So far he is having trouble finding an employee with strong business and strategic planning skills who can help him improve the processes at his existing company, and help him plan and implement his IT startups."
+
