fc6956a6d9f604ba20f7dc417566551785209d3d
Peter Street <PeterBStreet@gmail.com>
2018-02-01 11:34:09 -0700

    Milestone 2 final

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,30 +1,45 @@
-
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>milestone-2</title>
-</head>
-<body>
-<h1>PWP Milestone 2</h1>
-<h2>Content Strategy</h2>
-<div>
-	<p>
- I am not interested in adding music or video to the website. If the viewer is using a laptop or desk top I would like my splash page to have my contact information, linkedin, and github links centered on the page. Surrounding my contact information and professional social media links, I would like to have a links to "skills" including resume, web page projects, engineering projects, Project Management Highlights, Lean Six Sigma Process Improvement project highlights, and contact me. If the user is using a device that has a smaller view port, I would like the web page to have my contact information and professional links at top center and the skills links presented in two columns with three rows below the contact information.
-	</p>
-</div>
-	<div>
-		<p>
- Ideally, the splash page will have a neutral, modern, professional background. The contact info and "skills" text overlay, of the background image, should be sophisticated yet easy discernable and ultimatly topical.
-		</p>
-	</div>
-	<div>
-		<p>
- The "skills" boxes are focused on the referenced topic and the content will clearly and professionally demonstrates successful completion of projects associated with the topic. Ultimatly, I would like to include a contact me link. Functioning like the other links the contact me link will open the visitor's preferred e-mail and auto populate my e-mail in the "To:" field.
-		</p>
-	</div>
-<h2>Wireframes</h2>
-	<img src=*/page_1.png" alt="Desktop or Laptop web page wireframe or layout.">
-	<img src=*/page_2.png" alt="Mobile web page wireframe or layout page 1.">
-	<img src=*/page_3.png" alt="Mobile web page wireframe or layout page 2.">
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>milestone-2</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2</h1>
+		<h2>Content Strategy</h2>
+		<div>
+			<p>
+				I am not interested in adding music or video to the website. If the viewer is using a laptop or desk top I
+				would like my splash page to have my contact information, linkedin, and github links centered on the page.
+				Surrounding my contact information and professional social media links, I would like to have a links to
+				"skills" including resume, web page projects, engineering projects, Project Management Highlights, Lean Six
+				Sigma Process Improvement project highlights, and contact me. If the user is using a device that has a
+				smaller view port, I would like the web page to have my contact information and professional links at top
+				center and the skills links presented in two columns with three rows below the contact information.
+			</p>
+		</div>
+		<div>
+			<p>
+				Ideally, the splash page will have a neutral, modern, professional background. The contact info and "skills"
+				text overlay, of the background image, should be sophisticated yet easy discernable and ultimatly topical.
+			</p>
+		</div>
+		<div>
+			<p>
+				The "skills" boxes are focused on the referenced topic and the content will clearly and professionally
+				demonstrates successful completion of projects associated with the topic. Ultimatly, I would like to include
+				a contact me link. Functioning like the other links the contact me link will open the visitor's preferred
+				e-mail and auto populate my e-mail in the "To:" field.
+			</p>
+		</div>
+		<h2>Wireframes</h2>
+		<div>
+			<img src=*/page_1.png" alt="Desktop or Laptop web page wireframe or layout.">
+		</div>
+		<div>
+			<img src=*/page_2.png" alt="Mobile web page wireframe or layout page 1.">
+		</div>
+		<div>
+			<img src=*/page_3.png" alt="Mobile web page wireframe or layout page 2.">
+		</div>
+	</body>
 </html>
