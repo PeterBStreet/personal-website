b81463c4ea5af9a94327e9216d5b8860c1de5b40
Peter Street <PeterBStreet@gmail.com>
2018-02-20 21:45:19 -0700

    PWP Re-Do Update

--- public_html/index.php
+++ public_html/index.php
@@ -40,30 +40,14 @@
 			</nav>
 		</div>
 
-		<form class="container my-3 mx-5">
-			<div class="form-group">
-				<label for="userContactForm">Your Email address</label>
-				<input type="email" class="form-control" id="userInputEmail" aria-describedby="emailHelp" placeholder="Enter your email please">
-				<small id="emailHelp" class="form-text text-muted">I will sell your email for as much money as I can!</small>
-			</div>
 
-			<div class="form-group">
-				<label for="userContactForm">Please use your social security number as your password</label>
-				<input type="password" class="form-control" id="userInputPassword" aria-describedby="passwordHelp" placeholder="Enter your password please"> <small id="passwordHelp" class="form-text text-muted">I have already sold your social security number!</small>
-			</div>
-
-			<div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-			</div>
-
-		</form>
 		<!--Here I created a div and created a container. my is margins top and bottom mx is margins left to right -->
 		<div class="container my-3 mx-5">
 			<!-- navbar with "Bootstrap Challenge" Title and 4 "links" aligned right.
 			 Here I created a navbar that will expanded when medium. with light text and background and rounded border-->
 			<nav class="navbar navbar-expand-md navbar-info justify-content-center bg-light border rounded">
 				<!-- the  navbar-brand gets our text in the nav bar -->
-				<a class="navbar-brand" href="#">Bootstrap Challenge</a>
+				<a class="navbar-brand" href="#">AbqGig!</a>
 				<!--Here I created buttons that collapse in white. Again sample  -->
 				<button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarToggler">
 					<span class="navbar-toggler-icon"></span>
@@ -74,51 +58,55 @@
 					The mt settings affect the thickness of the navbar-->
 					<ul class="navbar-nav ml-auto mt-0 mt-sm-0">
 						<li class="nav-item">
-							<a class="nav-link" href="#">Link1</a>
+							<a class="nav-link" href="#">What are we doing?</a>
 						</li>
 						<li class="nav-item align-items-center">
-							<a class="nav-link" href="#">Link2</a>
+							<a class="nav-link" href="#">Our Strategies:</a>
+						</li>
+
+						<li class="nav-item">
+							<a class="nav-link" href="#">Why are we doing it?</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">Link3</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Link4</a>
+							<a class="nav-link" href="#">Who is AbqGig</a>
 						</li>
 					</ul>
 				</div>
 			</nav>
-			<!-- Hello, world! Container with text and "Learn More" button
-			Initially I got the text and button and only the button was correct
-			w3schools was my resource for the fix which places the JTRON inside a container
-			 The jumbotron is not
-			 the Margin Top is set to five (5)
-			 which I size using the py to control the container hight, px to control the box width, and mt for the container spacing
-			 additionally I want to note the Jolynn helped me with the text sizing.-->
-			<div class="jumbotron container mt-5">
-				<h1 class="display-4">Hello, world!</h1>
-				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
-					to featured content or information.</p>
-				<p class="lead">
-					<a class="btn btn-primary btn-md" href="#" role="button">Learn more</a>
+			<div class="jumbotron container mt-5 bg-primary text-white">
+				<h1 class="display-4">AbqGig!</h1>
+				<p class="lead">Making Albuquerque a Smart Gigabit City</p>
+			</div>
+			<div class="jumbotron container mt-5 bg-secondary text-white">
+				<h1 class="display-4">What are we doing?</h1>
+				<p class="lead">AbqGig is a community broadband working group between the technology industry, academic institutions, research labs, local governments, businesses, startups, and residents working togeather to improve the high-speed broadband infrastructure and service in Albuquerque, NM</p>
 				</p>
 			</div>
+			<div class="jumbotron container mt-5 bg-info text-white">
+				<h1 class="display-4">Our Strategies:</h1>
+				<p class="lead">Educate Private and Public Sector Stakeholders</p>
+				<p class="lead">Locate new technologies in emergent industries that drive next gen gig applications</p>
+				<p class="lead">Bringing modern wireless technology to underserved communities</p>
+			</div>
+				<div class="jumbotron container mt-5 bg-success text-white">
+					<h1 class="display-4">Why are we doing it?</h1>
+					<p class="lead">To support policies that improve broadband competition by encouraging and removing barriers to the deployment of high-speed networks.</p>
+				</p>
+			</div>
+
+
+
 		</div>
 
-		<!-- Middle section with image left and text right
-		with help from Jolynn-->
 		<div class="container-fluid bg-light my-4 p-5">
 			<div class="row align-items-center">
 				<div class="col-lg-4 text-center">
 					<a href="https://placeimg.com/250/250/nature"><img src="https://placeimg.com/250/250/nature"></a>
 				</div>
 				<div class="col-lg-8 align-self-center p-4">
-					<p>Chuanjiesaurus Petrobrasaurus Teratophoneus Pisanosaurus Archaeodontosaurus Xenoceratops Atlantosaurus
-						Longosaurus Succinodon Hypsibema Nedoceratops Fusuisaurus Siamosaurus Jianchangosaurus Teinurosaurus
-						Rahona Xiongguanlong Dongyangosaurus<br><br>Abydosaurus Neuquensaurus Kaatedocus Genusaurus
-						Camarasaurus Dravidosaurus Zhejiangosaurus Aralosaurus Secernosaurus Doratodon Brontosaurus
-						Bambiraptor Morosaurus Loricatosaurus Sinovenator Variraptor Dandakosaurus Prenoceratops
-						Krzyzanowskisaurus Tianzhenosaurus Huehuecanauhtlus Lapampasaurus.</p>
+					<h1>Mozilla and NSF </h1>
+					<h2>Wireless Innovation for a Networked Society</h2>
+					<h3>AbqGig | BarelasGig | Honorable Mention</h3>
 				</div>
 			</div>
 		</div>
@@ -128,33 +116,61 @@
 				<div class="col-lg-3 my-4 text-center">
 					<a href="https://placeimg.com/174/174/nature"><img src="https://placeimg.com/174/174/nature" class="rounded-circle"></a>
 					<div class="d-inline-flex p-2 mx-3 my-4 justify-content-center border border rounded bg-light">
-						<p class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan dignissim nulla ac scelerisque. Praesent scelerisque pretium nulla. Etiam vel egestas mauris. Sed lacinia vitae felis vel vulputate. Sed quis metus et sapien fermentum auctor mattis ut orci. Cras dapibus nisi venenatis consequat consectetur.</p>
+						<p class="small">Ric Aguilar</p>
 					</div>
 
 				</div>
 				<div class="col-lg-3 my-4 text-center">
 					<a href="https://placeimg.com/175/175/animals"><img src="https://placeimg.com/175/175/animals" class="rounded-circle"></a>
 					<div class="d-inline-flex p-2 mx-3 my-4 justify-content-center border border rounded bg-light">
-						<p class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan dignissim nulla ac scelerisque. Praesent scelerisque pretium nulla. Etiam vel egestas mauris. Sed lacinia vitae felis vel vulputate. Sed quis metus et sapien fermentum auctor mattis ut orci. Cras dapibus nisi venenatis consequat consectetur.</p>
+						<p class="small">Tom Feldman</p>
 					</div>
 				</div>
 
 				<div class="col-lg-3 my-4 text-center">
 					<a href="https://placeimg.com/175/175/architecture"><img src="https://placeimg.com/175/175/architecture" class="rounded-circle"></a>
 					<div class="d-inline-flex p-2 mx-3 my-4 justify-content-center border border rounded bg-light">
-						<p class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan dignissim nulla ac scelerisque. Praesent scelerisque pretium nulla. Etiam vel egestas mauris. Sed lacinia vitae felis vel vulputate. Sed quis metus et sapien fermentum auctor mattis ut orci. Cras dapibus nisi venenatis consequat consectetur.</p>
+						<p class="small">Michael Sanchez</p>
 					</div>
 				</div>
 
 				<div class="col-lg-3 my-4 text-center">
 					<a href="https://placeimg.com/175/175/tech"><img src="https://placeimg.com/175/175/tech" class="rounded-circle"></a>
 					<div class="d-inline-flex p-2 mx-3 my-4 justify-content-center border border rounded bg-light">
-						<p class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan dignissim nulla ac scelerisque. Praesent scelerisque pretium nulla. Etiam vel egestas mauris. Sed lacinia vitae felis vel vulputate. Sed quis metus et sapien fermentum auctor mattis ut orci. Cras dapibus nisi venenatis consequat consectetur.</p>
+						<p class="small">Peter Street</p>
 					</div>
 				</div>
 			</div>
 		</div>
+		<form class="container my-3 mx-5">
+			<div class="form-group">
+				<label for="userContactForm">Your Email address</label>
+				<input type="email" class="form-control" id="userInputEmail" aria-describedby="emailHelp" placeholder="Enter your email please">
+				<small id="emailHelp" class="form-text text-muted">I will sell your email for as much money as I can!</small>
+			</div>
 
+			<div class="form-group">
+				<label for="userContactForm">Please use your social security number as your password</label>
+				<input type="password" class="form-control" id="userInputPassword" aria-describedby="passwordHelp" placeholder="Enter your password please"> <small id="passwordHelp" class="form-text text-muted">I have already have a buyer for your social security number!</small>
+			</div>
+			<div class="container">
+				<h2>Please type your message here!</h2>
+				<p>We will certainly ignore all requests submitted:</p>
+				<form>
+					<div class="form-group">
+						<label for="comment">Message:</label>
+						<textarea class="form-control" rows="5" id="comment"></textarea>
+					</div>
+				</form>
+			</div>
+			<div>
+
+			</div>
+			<div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+
+		</form>
 		<!-- Here is the bottom navbar (found on www.getbootstrap.com) with adjusted float, not fixed,text is dark, with a dark background, and justified center. Not clear what the navbar-brand or href do but when I remove the "elements" the text is dark so I left them in! Additionally I can not get the bottom bar to extend to the sides of the page like I did the top bar.-->
 <div>
 	<nav class="navbar navbar-fixed-bottom navbar-dark bg-dark justify-content-center">
